d7ff28706d36c33fc2985ea8a1e32b8660e3f9f7
Mingze Xiao <mxiao666@gmail.com>
2016-11-14 15:35:44 -0600

    update q45

---
+++ potd-q45/q45.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void function1(int x){
+    x ? (cout << "Hail to the Orange.\n") : cout << "Nope.\n";
+}
+
+void function2(char a){
+    if(a == 64)
+        cout << "Hail to the Blue.\n";
+    else
+        cout << "Not correct.\n";
+}
+
+void function3(int x, int y){
+    if(++x == 3)
+        cout << "Hail Alma Mater,\n";
+    else
+        cout << "Try again.\n";
+    if(y++ == 2)
+        cout << "Ever so true.\n";
+    else
+        cout << "Not quite.\n";
+}
+
+void function4(string str){
+    int x = str.rfind("c++");
+    if(x == 8)
+        cout << "We love no other,\n";
+    else
+        cout << "Something is not right.\n";
+}
+
+void function5(int x){
+    x &= 1;
+    if(x^1)
+        cout << "So let our motto be\n";
+    else
+        cout << "Oops.\n";
+}
+
+void function6(int *ptr){
+    if(2[ptr] == 5)
+        cout << "Victory, Illinois, Varsity.\n";
+    else
+        cout << "That's not right.\n";
+}
+
+int main(){
+    // I-L-L !
+    function1(1);
+
+    function2(char(64));
+
+    function3(2, 2);
+
+    function4("01234567c++");
+    
+    function5(0);
+    
+    int* p = new int[3];
+    p[0] = 0;
+    p[1] = 1;
+    p[2] = 5;
+    function6(p);
+
+    return 0;
+    // I-N-I !
+}
